9ddd2688df8a8978b14ff97f0550752f04b5eb84
AxelFeL <91774548+AxelFeL@users.noreply.github.com>
2022-02-19 14:25:36 +0700

    Create Main.php

---
+++ src/AxelFeL/DeathPosition/Main.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AxelFeL\DeathPosition;
+
+use pocketmine\player\Player;
+use pocketmine\Server;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener {
+  
+  public function onEnable() : void {
+    $this->getLogger()->info("Plugin Enabled By AxelFeL!");
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function onPlayerDeath(PlayerDeathEvent $event) {
+    $player = $event->getPlayer();
+    $x = $player->getPosition()->getFloorX();
+    $y = $player->getPosition()->getFloorY();
+    $z = $player->getPosition()->getFloorZ();
+    $player->sendMessage("§eYour death position on coordinates: §a".$x." ".$y." ".$z);
+  }
+}
